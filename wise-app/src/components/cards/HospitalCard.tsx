b5cf9b10c3ba906c39939ba282082f0da81cd54b
Shubham Vyavhare <shubhamvyavhare30@gmail.com>
2024-06-27 23:10:40 +0530

    Developed Nearby Hospitals UI

--- wise-app/src/components/cards/HospitalCard.tsx
+++ wise-app/src/components/cards/HospitalCard.tsx
@@ -4,27 +4,21 @@ import Ionicons from "react-native-vector-icons/Ionicons";
 import { Divider } from "@rneui/base";
 import { COLORS } from "@/src/constants/colors";
 import CustomText from "../custom-widgets/CustomText";
-const HospitalCard = (props: any) => {
-  const rideData = {
-    name: "Sigma Hospital",
-    email: "ram@gmail.com",
-    from: "Somwhere",
-    timeLabel: "200",
-    to: "something",
-    gender: "female",
-    vehicle: "tvs",
-    phone: "9090443433",
-    dorLabel: "43433",
-    category: "203",
-    amount: 20,
-  };
-  const { name } = rideData;
+import { Hospital } from "@/src/app/(app)/hospitals";
+
+interface ChildProps {
+  hospitalData: Hospital;
+}
+
+const HospitalCard: React.FC<ChildProps> = ({ hospitalData }) => {
+  const { name, address, contact, distance, coordinates, doctor } =
+    hospitalData;
   return (
     <View style={styles.cardContainer}>
       <View style={{ flexDirection: "row", gap: 5 }}>
         <Image
-          source={require("../../../assets/images/illustrations/slogan-women.png")}
-          style={styles.hospitalImage}
+          style={styles.image}
+          source={require("../../../assets/images/illustrations/hospitals/hospital.png")}
         />
         <View style={styles.cardContent}>
           <CustomText label={name} customStyle={styles.nameText} />
@@ -34,31 +28,24 @@ const HospitalCard = (props: any) => {
       <Divider />
       <View style={styles.contactHeader}>
         <CustomText label="Address" customStyle={styles.infoLabel} />
-        <CustomText label="Child Hospital" customStyle={styles.infoText} />
+        <CustomText label={address} customStyle={styles.infoText} />
       </View>
-      <Divider />
-      <View style={styles.contactHeader}>
-        <CustomText label="Category" customStyle={styles.infoLabel} />
-        <CustomText
-          label="Darga Road, Front of DMart, Aurangabad"
-          customStyle={styles.infoText}
-        />
-      </View>
+
       <Divider />
       <View style={styles.contactHeader}>
         <CustomText label="Doctor Name" customStyle={styles.infoLabel} />
-        <CustomText label="Dr. Sunil Kuyare" customStyle={styles.infoText} />
+        <CustomText label={doctor} customStyle={styles.infoText} />
       </View>
       <Divider />
 
       <View style={styles.contactHeader}>
         <CustomText label="Contact No." customStyle={styles.infoLabel} />
-        <CustomText label="80803434343" customStyle={styles.infoText} />
+        <CustomText label={contact} customStyle={styles.infoText} />
       </View>
       <Divider />
       <View style={styles.contactHeader}>
         <CustomText label="Distance" customStyle={styles.infoLabel} />
-        <CustomText label="1 KM" customStyle={styles.infoText} />
+        <CustomText label={distance} customStyle={styles.infoText} />
       </View>
     </View>
   );
@@ -67,19 +54,15 @@ const HospitalCard = (props: any) => {
 const styles = StyleSheet.create({
   cardContainer: {
     backgroundColor: COLORS.light,
-    marginHorizontal: 10,
     borderRadius: 10,
     paddingVertical: 20,
     paddingHorizontal: 6,
     marginVertical: 8,
-    marginLeft: "1.8%",
-    marginRight: "1.8%",
     shadowOffset: { width: 0, height: 2 },
     elevation: 5,
     shadowRadius: 0.84,
     shadowOpacity: 0.15,
     shadowColor: "#52006A",
-    marginTop: 100,
   },
 
   hospitalImage: {
@@ -115,6 +98,10 @@ const styles = StyleSheet.create({
     fontSize: 15,
     color: COLORS.dark,
     width: "45%",
+  },
+  image: {
+    height: 50,
+    width: 50,
   },
 });
 
