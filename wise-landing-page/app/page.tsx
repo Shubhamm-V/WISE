d094eca260a2fa1619d0fc0bc2fd8516895c8ccf
Shubham Vyavhare <shubhamvyavhare30@gmail.com>
2024-07-24 22:14:56 +0530

    fixed minor issue for production

--- wise-landing-page/app/page.tsx
+++ wise-landing-page/app/page.tsx
@@ -1,8 +1,6 @@
 import Banner from "./components/Banner/index";
 import Features from "./components/Features/index";
-
 import Register from "./components/Register/index";
-
 import Overview from "./components/Overview/index";
 import FAQ from "./components/FAQ/index";
 
