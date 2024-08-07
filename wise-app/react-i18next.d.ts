96a5b7c03c9479ceec54562d68e357662353a8d2
Shubham Vyavhare <shubhamvyavhare30@gmail.com>
2024-08-07 22:52:34 +0530

    Removed unnecessary libraries

--- wise-app/react-i18next.d.ts
+++
@@ -1,9 +0,0 @@
-import "react-i18next";
-
-declare module "react-i18next" {
-  interface Resources {
-    en: typeof import("./src/translations/en.json");
-    hi: typeof import("./src/translations/hi.json");
-    // Add other languages here
-  }
-}
