7e0b4762b048e0e66925f2e5dfc5a4686360f729
Shubham Vyavhare <shubhamvyavhare30@gmail.com>
2024-08-14 20:50:02 +0530

    Fixed minor bug

--- wise-web/src/firebaseConfig.js
+++ wise-web/src/firebaseConfig.js
@@ -8,13 +8,13 @@ import {
 import { getFirestore, collection } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB33TsU052_1UNq_kv2cERuSUDwU1NnL8A",
-  authDomain: "women-health-empowerment.firebaseapp.com",
-  projectId: "women-health-empowerment",
-  storageBucket: "women-health-empowerment.appspot.com",
-  messagingSenderId: "999945649944",
-  appId: "1:999945649944:web:a08d1c532f09ff23dd286d",
-  measurementId: "G-ZQ2Y56SXK2",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSEGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
