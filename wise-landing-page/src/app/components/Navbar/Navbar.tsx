441320315e270c9be4992eff7a71d81799019434
Shubham Vyavhare <shubhamvyavhare30@gmail.com>
2024-09-06 10:55:50 +0530

    Added pre-release notification

--- wise-landing-page/src/app/components/Navbar/Navbar.tsx
+++ wise-landing-page/src/app/components/Navbar/Navbar.tsx
@@ -1,8 +1,8 @@
 "use client";
 import { Disclosure } from "@headlessui/react";
 import Link from "next/link";
-import React from "react";
-import { Bars3Icon } from "@heroicons/react/24/outline";
+import React, { useState } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import Drawer from "./Drawer";
 import Drawerdata from "./Drawerdata";
 import Contactusform from "./Contactus";
@@ -27,77 +27,95 @@ function classNames(...classes: string[]) {
 }
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = React.useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [showNotification, setShowNotification] = useState(true);
 
   return (
-    <Disclosure as="nav" className="navbar">
-      <>
-        <div className="mx-auto max-w-7xl p-3 md:p-4 lg:px-8">
-          <div className="relative flex h-12 sm:h-20 items-center">
-            <div className="flex flex-1 items-center sm:justify-between">
-              {/* LOGO */}
-
-              <div className="flex flex-shrink-0 items-center border-right">
-                <Link
-                  href="/"
-                  className="text-4xl flex sm:text-4xl font-semibold text-black"
-                >
-                  <Image
-                    src="/images/icon.png"
-                    alt="app-icon"
-                    height={41}
-                    width={41}
-                  />
-                  <label className="text-primary mt-0">WISE</label>
-                </Link>
-              </div>
-
-              {/* LINKS */}
-
-              <div className="hidden lg:flex items-center border-right ">
-                <div className="flex justify-end space-x-4">
-                  {navigation.map((item) => (
-                    <Link
-                      key={item.name}
-                      href={item.href}
-                      className={classNames(
-                        item.current
-                          ? "bg-gray-900"
-                          : "navlinks hover:text-black",
-                        "px-3 py-4 rounded-md text-lg font-normal"
-                      )}
-                      aria-current={item.href ? "page" : undefined}
-                    >
-                      {item.name}
-                    </Link>
-                  ))}
-                </div>
-              </div>
-              {/* <button className='hidden lg:flex justify-end text-xl font-semibold bg-transparent py-4 px-6 lg:px-12 navbutton rounded-full hover:bg-navyblue hover:text-white'>Contact us</button> */}
-              <Contactusform />
-            </div>
-
-            {/* DRAWER FOR MOBILE VIEW */}
-
-            {/* DRAWER ICON */}
-
-            <div className="block lg:hidden">
-              <Bars3Icon
-                className="block h-10 w-10"
-                aria-hidden="true"
-                onClick={() => setIsOpen(true)}
-              />
-            </div>
-
-            {/* DRAWER LINKS DATA */}
-
-            <Drawer isOpen={isOpen} setIsOpen={setIsOpen}>
-              <Drawerdata />
-            </Drawer>
+    <>
+      {showNotification && (
+        <div className=" bg-lightprimary">
+          <div className="mx-auto max-w-7xl p-3 flex justify-between lg:px-8">
+            <label className="text-primary text-sm font-semibold lg:text-base">
+              The app is currently in closed testing and will be published on
+              the Play Store in mid-October
+            </label>
+            <XMarkIcon
+              color="#461A71"
+              className="cursor-pointer font-bold h-10 w-10 lg:h-6 lg:w-6"
+              onClick={() => setShowNotification((prev) => !prev)}
+            />
           </div>
         </div>
-      </>
-    </Disclosure>
+      )}
+      <Disclosure as="nav" className="navbar">
+        <>
+          <div className="mx-auto max-w-7xl p-3 md:p-4 lg:px-8">
+            <div className="relative flex h-12 sm:h-20 items-center">
+              <div className="flex flex-1 items-center sm:justify-between">
+                {/* LOGO */}
+
+                <div className="flex flex-shrink-0 items-center border-right">
+                  <Link
+                    href="/"
+                    className="text-4xl flex sm:text-4xl font-semibold text-black"
+                  >
+                    <Image
+                      src="/images/icon.png"
+                      alt="app-icon"
+                      height={41}
+                      width={41}
+                    />
+                    <label className="text-primary mt-0">WISE</label>
+                  </Link>
+                </div>
+
+                {/* LINKS */}
+
+                <div className="hidden lg:flex items-center border-right ">
+                  <div className="flex justify-end space-x-4">
+                    {navigation.map((item) => (
+                      <Link
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current
+                            ? "bg-gray-900"
+                            : "navlinks hover:text-black",
+                          "px-3 py-4 rounded-md text-lg font-normal"
+                        )}
+                        aria-current={item.href ? "page" : undefined}
+                      >
+                        {item.name}
+                      </Link>
+                    ))}
+                  </div>
+                </div>
+                {/* <button className='hidden lg:flex justify-end text-xl font-semibold bg-transparent py-4 px-6 lg:px-12 navbutton rounded-full hover:bg-navyblue hover:text-white'>Contact us</button> */}
+                <Contactusform />
+              </div>
+
+              {/* DRAWER FOR MOBILE VIEW */}
+
+              {/* DRAWER ICON */}
+
+              <div className="block lg:hidden">
+                <Bars3Icon
+                  className="block h-10 w-10"
+                  aria-hidden="true"
+                  onClick={() => setIsOpen(true)}
+                />
+              </div>
+
+              {/* DRAWER LINKS DATA */}
+
+              <Drawer isOpen={isOpen} setIsOpen={setIsOpen}>
+                <Drawerdata />
+              </Drawer>
+            </div>
+          </div>
+        </>
+      </Disclosure>
+    </>
   );
 };
 
